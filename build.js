7db23d88bb0e844ca6ad123e6bde01839a8a90da
Pouya Eghbali <persian.writer@gmail.com>
2020-01-19 20:47:17 +0100

    Added build.js

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const fs = requre("fs");
-const path = requre("path");
-const { execSync } = requre("child_process");
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
 const buildDir = path.join(__dirname, "build");
 fs.mkdirSync(buildDir, { recursive: true });
