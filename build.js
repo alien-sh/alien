af87c560cde2b1c0833f33cab940724978b91748
Pouya Eghbali <persian.writer@gmail.com>
2020-01-19 20:51:59 +0100

    Added build.js

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { execSync } = require("child_process");
 
 const buildDir = path.join(__dirname, "build");
-fs.mkdirSync(buildDir, { recursive: true });
+fs.mkdirSync(buildDir, { recursive: true }, () => {});
 
 execSync("cmake ..", { cwd: buildDir });
 execSync("cmake --build .", { cwd: buildDir });
